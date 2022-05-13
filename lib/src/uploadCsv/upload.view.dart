9246478bfabe09cfb8222eb921eaf60a32e32333
ksanbal <dev.ksanbal@gmail.com>
2022-05-13 11:16:24 +0900

    메인 페이지 디자인 수정

--- lib/src/uploadCsv/upload.view.dart
+++ lib/src/uploadCsv/upload.view.dart
@@ -11,65 +11,81 @@ class UploadCsvView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.grey[800],
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: oNeonContainer(
+          borderWidth: 2,
           borderRadius: BorderRadius.circular(15),
           spreadColor: Colors.cyan.withOpacity(0.6),
           containerColor: Colors.black,
           lightBlurRadius: 10,
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                oNeonText(
-                  text: '평생 든든 뽑기!',
-                  fontWeight: FontWeight.bold,
-                  textSize: 60,
-                  spreadColor: Colors.pink,
-                ),
-                const SizedBox(height: 100),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: 300,
-                  ),
-                  child: oFlickerNeonContainer(
-                    flickerTimeInMilliSeconds: 1000,
-                    borderRadius: BorderRadius.circular(15),
-                    borderWidth: 3,
-                    lightSpreadRadius: 10,
-                    lightBlurRadius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: TextButton.icon(
-                        onPressed: () async {
-                          List<String> participateList = await _uploadRepository.getListFromUploadCsv();
-                          if (participateList.isNotEmpty) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => SettingView(
-                                  participateList: participateList,
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network('https://i.gifer.com/yi.gif'),
+                      oNeonText(
+                        text: '평생 든든 뽑기!',
+                        fontWeight: FontWeight.bold,
+                        textSize: 60,
+                        spreadColor: Colors.pink,
+                      ),
+                      const SizedBox(height: 100),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          maxWidth: 500,
+                        ),
+                        child: oFlickerNeonContainer(
+                          flickerTimeInMilliSeconds: 1000,
+                          borderWidth: 2,
+                          borderRadius: BorderRadius.circular(15),
+                          lightSpreadRadius: 10,
+                          lightBlurRadius: 30,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: TextButton.icon(
+                              onPressed: () async {
+                                List<String> participateList = await _uploadRepository.getListFromUploadCsv();
+                                if (participateList.isNotEmpty) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => SettingView(
+                                        participateList: participateList,
+                                      ),
+                                    ),
+                                  );
+                                }
+                              },
+                              icon: const Icon(
+                                Icons.file_upload_outlined,
+                                color: Colors.white,
+                                size: 35,
+                              ),
+                              label: const Text(
+                                '명단 업로드',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
                                 ),
                               ),
-                            );
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.file_upload_outlined,
-                          color: Colors.white,
-                          size: 35,
-                        ),
-                        label: const Text(
-                          '명단 업로드',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: oFlickerNeonText(
+                    text: 'powered by dev.ksanbal',
+                    spreadColor: Colors.pink,
+                    textColor: Colors.white,
                   ),
                 ),
               ],

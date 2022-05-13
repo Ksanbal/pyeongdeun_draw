073536d7875ac245636918c4e6c00edaf343259d
ksanbal <dev.ksanbal@gmail.com>
2022-05-13 14:03:13 +0900

    구글 애널리틱스 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pyeongdeun_draw/firebase_options.dart';
 import 'package:pyeongdeun_draw/src/uploadCsv/upload.view.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
+  FirebaseAnalytics.instance.logAppOpen();
 }
 
 class MyApp extends StatelessWidget {
